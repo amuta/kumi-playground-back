73a7dc1d20e08c82688e2e0a80c919e192a6a477
Andre Muta <andremuta@gmail.com>
2025-10-03 00:22:40 -0300

    minv

--- app/services/kumi_compile.rb
+++ app/services/kumi_compile.rb
@@ -1,11 +1,16 @@
 class KumiCompile
   def self.call(src)
-    ast = Kumi::Parser::TextParser.parse(src)
-    res = Kumi::Analyzer.analyze!(ast, side_tables: true)
-    js_src = res.state[:javascript_codegen_files]["codegen.js"]
+    begin
+      ast,_ = Kumi::Frontends::Text.load(src:)
+      res = Kumi::Analyzer.analyze!(ast, side_tables: true)
+      js_src = res.state[:javascript_codegen_files]["codegen.js"]
 
-    { ok: true, js_src: js_src }
-  rescue => e
-    { ok: false, errors: e.message }
+      { ok: true, js_src: js_src }
+    rescue => e
+    require "pry"
+    binding.pry
+      Rails.logger.error e
+      { ok: false, errors: e.message }
+    end
   end
 end
